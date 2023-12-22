45d259e7be4efe456abe0e8e884d1bbca1ca5d70
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2023-12-22 04:54:29 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4265,6 +4265,123 @@
                 destroy
             });
         }
+        function effect_init_effectInit(params) {
+            const {effect, swiper, on, setTranslate, setTransition, overwriteParams, perspective, recreateShadows, getEffectParams} = params;
+            on("beforeInit", (() => {
+                if (swiper.params.effect !== effect) return;
+                swiper.classNames.push(`${swiper.params.containerModifierClass}${effect}`);
+                if (perspective && perspective()) swiper.classNames.push(`${swiper.params.containerModifierClass}3d`);
+                const overwriteParamsResult = overwriteParams ? overwriteParams() : {};
+                Object.assign(swiper.params, overwriteParamsResult);
+                Object.assign(swiper.originalParams, overwriteParamsResult);
+            }));
+            on("setTranslate", (() => {
+                if (swiper.params.effect !== effect) return;
+                setTranslate();
+            }));
+            on("setTransition", ((_s, duration) => {
+                if (swiper.params.effect !== effect) return;
+                setTransition(duration);
+            }));
+            on("transitionEnd", (() => {
+                if (swiper.params.effect !== effect) return;
+                if (recreateShadows) {
+                    if (!getEffectParams || !getEffectParams().slideShadows) return;
+                    swiper.slides.each((slideEl => {
+                        const $slideEl = swiper.$(slideEl);
+                        $slideEl.find(".swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left").remove();
+                    }));
+                    recreateShadows();
+                }
+            }));
+            let requireUpdateOnVirtual;
+            on("virtualUpdate", (() => {
+                if (swiper.params.effect !== effect) return;
+                if (!swiper.slides.length) requireUpdateOnVirtual = true;
+                requestAnimationFrame((() => {
+                    if (requireUpdateOnVirtual && swiper.slides && swiper.slides.length) {
+                        setTranslate();
+                        requireUpdateOnVirtual = false;
+                    }
+                }));
+            }));
+        }
+        function effect_target_effectTarget(effectParams, $slideEl) {
+            if (effectParams.transformEl) return $slideEl.find(effectParams.transformEl).css({
+                "backface-visibility": "hidden",
+                "-webkit-backface-visibility": "hidden"
+            });
+            return $slideEl;
+        }
+        function effect_virtual_transition_end_effectVirtualTransitionEnd({swiper, duration, transformEl, allSlides}) {
+            const {slides, activeIndex, $wrapperEl} = swiper;
+            if (swiper.params.virtualTranslate && duration !== 0) {
+                let eventTriggered = false;
+                let $transitionEndTarget;
+                if (allSlides) $transitionEndTarget = transformEl ? slides.find(transformEl) : slides; else $transitionEndTarget = transformEl ? slides.eq(activeIndex).find(transformEl) : slides.eq(activeIndex);
+                $transitionEndTarget.transitionEnd((() => {
+                    if (eventTriggered) return;
+                    if (!swiper || swiper.destroyed) return;
+                    eventTriggered = true;
+                    swiper.animating = false;
+                    const triggerEvents = [ "webkitTransitionEnd", "transitionend" ];
+                    for (let i = 0; i < triggerEvents.length; i += 1) $wrapperEl.trigger(triggerEvents[i]);
+                }));
+            }
+        }
+        function EffectFade({swiper, extendParams, on}) {
+            extendParams({
+                fadeEffect: {
+                    crossFade: false,
+                    transformEl: null
+                }
+            });
+            const setTranslate = () => {
+                const {slides} = swiper;
+                const params = swiper.params.fadeEffect;
+                for (let i = 0; i < slides.length; i += 1) {
+                    const $slideEl = swiper.slides.eq(i);
+                    const offset = $slideEl[0].swiperSlideOffset;
+                    let tx = -offset;
+                    if (!swiper.params.virtualTranslate) tx -= swiper.translate;
+                    let ty = 0;
+                    if (!swiper.isHorizontal()) {
+                        ty = tx;
+                        tx = 0;
+                    }
+                    const slideOpacity = swiper.params.fadeEffect.crossFade ? Math.max(1 - Math.abs($slideEl[0].progress), 0) : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+                    const $targetEl = effect_target_effectTarget(params, $slideEl);
+                    $targetEl.css({
+                        opacity: slideOpacity
+                    }).transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+                }
+            };
+            const setTransition = duration => {
+                const {transformEl} = swiper.params.fadeEffect;
+                const $transitionElements = transformEl ? swiper.slides.find(transformEl) : swiper.slides;
+                $transitionElements.transition(duration);
+                effect_virtual_transition_end_effectVirtualTransitionEnd({
+                    swiper,
+                    duration,
+                    transformEl,
+                    allSlides: true
+                });
+            };
+            effect_init_effectInit({
+                effect: "fade",
+                swiper,
+                on,
+                setTranslate,
+                setTransition,
+                overwriteParams: () => ({
+                    slidesPerView: 1,
+                    slidesPerGroup: 1,
+                    watchSlidesProgress: true,
+                    spaceBetween: 0,
+                    virtualTranslate: !swiper.params.cssMode
+                })
+            });
+        }
         function initSliders() {
             if (document.querySelector(".news__slider")) {
                 const slider = document.querySelector(".news__slider");
@@ -4586,15 +4703,14 @@
                             observer: true,
                             observeParents: true,
                             slidesPerView: 2,
+                            slidesPerColumn: 1,
                             observeSlideChildren: true,
                             spaceBetween: 30,
-                            loop: true,
                             speed: 800,
-                            slidesPerColumn: 3,
                             autoHeight: false,
                             breakpoints: {
                                 320: {
-                                    slidesPerView: 1.2,
+                                    slidesPerView: 1.5,
                                     spaceBetween: 10
                                 },
                                 500: {
@@ -4924,6 +5040,29 @@
                     mobileSlider();
                 }));
             }
+            if (document.querySelector(".hero__slider")) new core(".hero__slider", {
+                modules: [ Pagination, EffectFade ],
+                observer: true,
+                observeParents: true,
+                slidesPerView: 1,
+                spaceBetween: 0,
+                speed: 800,
+                effect: "fade",
+                fadeEffect: {
+                    crossFade: true
+                },
+                pagination: {
+                    el: ".swiper-pagination_hero",
+                    clickable: true,
+                    renderBullet: function(index, className) {
+                        return '<span class="' + className + '">' + (index + 1) + "</span>";
+                    }
+                },
+                navigation: {
+                    nextEl: ".swiper-button-next_current-salaries"
+                },
+                on: {}
+            });
         }
         window.addEventListener("load", (function(e) {
             initSliders();
@@ -5030,9 +5169,17 @@
         const showContactsBtns = document.querySelectorAll(".item-info__show-contact");
         if (showContactsBtns) showContactsBtns.forEach((showContactsBtn => {
             showContactsBtn.addEventListener("click", (() => {
+                console.log(showContactsBtn.parentNode);
                 showContactsBtn.parentNode.classList.add("show-contact-active");
             }));
         }));
+        const quickJumpBtns = document.querySelectorAll(".quick-jump__button");
+        if (quickJumpBtns.length) {
+            const searchInput = document.querySelector(".hero__input");
+            for (const quickJumpBtn of quickJumpBtns) quickJumpBtn.addEventListener("click", (event => {
+                searchInput.value = quickJumpBtn.children[1].textContent;
+            }));
+        }
         window["FLS"] = false;
         isWebp();
         menuInit();
